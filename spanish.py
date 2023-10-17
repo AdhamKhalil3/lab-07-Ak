3a85e6f37ba49ff89a308928332e59562399ef8c
adham khalil <adhamkhalil@Adhams-MBP.lan>
2023-10-16 21:24:23 -0400

    spanish PY version

---
+++ spanish.py
@@ -0,0 +1,9 @@
+english = input("Give me a word in English ?")
+if english == "cat":
+    print ("gato")
+if english == "dog":
+    print("perro")
+if english == "horse":
+    print("caballo")
+else:
+    print("no entiendo")
