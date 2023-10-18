43731d9dfc35545a6c2d56029cdae22124df6671
adham khalil <adhamkhalil@Adhams-MacBook-Pro.local>
2023-10-18 16:10:51 -0400

    updated calc py

---
+++ oddcalc.py
@@ -0,0 +1,11 @@
+firstnumber = input("Give me a number , any number")
+secondnum = input("Okay, Give me a number , any number")
+operation = input("And what operation do you want (mul/div/mod)")
+if operation == "mul":
+    print == firstnumber * secondnum; 
+if (operation == "div"):
+    print = firstnumber / secondnum; 
+if (operation =="mod"):
+    print = firstnumber % secondnum
+else:
+    print = "INVAILD OPERATION"
